e0861bfcf2f225e43c833c97884b1e2c434fcd7c
lazantha <sglasanthapradeep@gmail.com>
2024-02-16 09:29:49 +0530

    updated session through contact method

--- sessions/app/Http/Controllers/adminController.php
+++ sessions/app/Http/Controllers/adminController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Session;
 use Illuminate\Http\Request;
 
 class adminController extends Controller
@@ -15,8 +15,31 @@ class adminController extends Controller
     }
     
     public function signIn(Request $request){
-        return $request;
         
+        Session::put('email',$request->email);
+        Session::put('password',$request->password);
+
+        return redirect()->route('contact_page');
+
+        
+    }
+    public function contact(){
+        
+        $email=session('email');
+        $password=session('password');
+        if(!empty($email && !empty($password))){
+            return view('contact',compact('email','password'));
+
+        }
+        else{
+            return redirect('home');
+        }
+
+
+
+        
+
+
     }
     
 }
